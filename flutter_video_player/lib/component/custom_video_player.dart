3aba12188c26b59ed07bfa56cbfc6a8e8f9571ca
jeongwongo <20123104a@gmail.com>
2023-12-26 21:12:05 +0900

    CustomVideoPlayer 위젯추가

---
+++ flutter_video_player/lib/component/custom_video_player.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
+
+class CustomVideoPlayer extends StatefulWidget {
+  final XFile video;
+  const CustomVideoPlayer({required this.video, super.key});
+
+  @override
+  State<CustomVideoPlayer> createState() => _CustomVideoPlayerState();
+}
+
+class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
+  VideoPlayerController? videoController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    initVideoController();
+  }
+
+  initVideoController() async {
+    videoController = VideoPlayerController.file(
+        //XFile -> file
+        File(widget.video.path));
+
+    await videoController!.initialize();
+
+    //videocontroller를 실행하고 나서 위젯 한 번 빌드해줘야되니까
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (videoController == null) {
+      return const CircularProgressIndicator();
+    }
+    return VideoPlayer(videoController!);
+  }
+}
