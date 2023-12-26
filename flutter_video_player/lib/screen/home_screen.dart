a15afd01588cd5dd6355d2aa7e962745d376045e
jeongwongo <20123104a@gmail.com>
2023-12-26 20:50:04 +0900

    ImagePicker로 갤러리에서 영상불러오기
    - 임시 화면 분기처리

--- flutter_video_player/lib/screen/home_screen.dart
+++ flutter_video_player/lib/screen/home_screen.dart
@@ -1,18 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  XFile? video;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
+    return Scaffold(body: video == null ? renderEmpty() : renderVideo());
+  }
+
+  Widget renderVideo() {
+    return Center(child: Text("video"));
+  }
+
+  Widget renderEmpty() {
+    return Container(
       width: MediaQuery.of(context).size.width,
       decoration: getBoxDecoration(),
-      child: const Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [_Logo(), SizedBox(height: 20), _AppName()]),
-    ));
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        _Logo(onTap: onLogoTap),
+        const SizedBox(height: 20),
+        _AppName()
+      ]),
+    );
+  }
+
+//로그클릭시, video 선택가능하도록
+  void onLogoTap() async {
+    final video = await ImagePicker().pickVideo(
+      source: ImageSource.gallery,
+    );
+
+    if (video != null) {
+      setState(() {
+        this.video = video;
+      });
+    }
   }
 }
 
@@ -25,11 +55,13 @@ BoxDecoration getBoxDecoration() {
 }
 
 class _Logo extends StatelessWidget {
-  const _Logo({super.key});
+  final VoidCallback onTap;
+  const _Logo({required this.onTap, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset("asset/image/logo.png");
+    return GestureDetector(
+        onTap: onTap, child: Image.asset("asset/image/logo.png"));
   }
 }
 
