375f0ded5435656ca48dc2414ae6809d29f96918
jeongwongo <20123104a@gmail.com>
2023-12-26 21:37:11 +0900

    videoController - play컨트롤 추가

--- flutter_video_player/lib/component/custom_video_player.dart
+++ flutter_video_player/lib/component/custom_video_player.dart
@@ -40,6 +40,82 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
     }
     return AspectRatio(
         aspectRatio: videoController!.value.aspectRatio,
-        child: VideoPlayer(videoController!));
+        child: Stack(children: [
+          VideoPlayer(videoController!),
+          _Controls(
+              onPlayPressed: onPlayPressed,
+              onReversePressed: onReversePressed,
+              onForwardPressed: onForwardPressed,
+              isPlaying: videoController!.value.isPlaying),
+          Positioned(
+            right: 0,
+            child: IconButton(
+                onPressed: () {},
+                iconSize: 30.0,
+                color: Colors.white,
+                icon: const Icon(Icons.photo_camera_back)),
+          )
+        ]));
+  }
+
+  void onReversePressed() {}
+
+  void onForwardPressed() {}
+
+  void onPlayPressed() {
+    //이미 실행중이면 중지
+    //실행중이 아니면 실행
+    setState(() {
+      if (videoController!.value.isPlaying) {
+        videoController!.pause();
+      } else {
+        videoController!.play();
+      }
+    });
+  }
+}
+
+class _Controls extends StatelessWidget {
+  final VoidCallback onPlayPressed;
+  final VoidCallback onReversePressed;
+  final VoidCallback onForwardPressed;
+  final bool isPlaying;
+
+  const _Controls(
+      {required this.onPlayPressed,
+      required this.onReversePressed,
+      required this.onForwardPressed,
+      required this.isPlaying,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black.withOpacity(0.5),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          renderIconButton(
+              onPressed: onReversePressed, iconData: Icons.rotate_left),
+          renderIconButton(
+              onPressed: onPlayPressed,
+              iconData: isPlaying ? Icons.pause : Icons.play_arrow),
+          renderIconButton(
+              onPressed: onForwardPressed, iconData: Icons.rotate_right),
+        ],
+      ),
+    );
+  }
+
+  Widget renderIconButton({
+    required VoidCallback onPressed,
+    required IconData iconData,
+  }) {
+    return IconButton(
+        onPressed: onPressed,
+        iconSize: 30.0,
+        color: Colors.white,
+        icon: Icon(iconData));
   }
 }
